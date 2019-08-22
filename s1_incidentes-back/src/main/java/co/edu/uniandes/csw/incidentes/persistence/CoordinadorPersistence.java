b5a5c4025f53cc72bb604ff4097ecbeaaf53b764
Juan Camilo <jc.castiblancor@uniandes.edu.co>
2019-08-22 07:34:12 -0500

    User ok

---
+++ s1_incidentes-back/src/main/java/co/edu/uniandes/csw/incidentes/persistence/CoordinadorPersistence.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidentes.persistence;
+
+import co.edu.uniandes.csw.incidentes.entities.CoordinadorEntity;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Juan Camilo Castiblanco
+ */
+@Stateless
+public class CoordinadorPersistence {
+    
+    public CoordinadorEntity create(CoordinadorEntity coordinador){
+        throw new java.lang.UnsupportedOperationException("Not suported yet");
+    }
+    
+    public CoordinadorEntity search(CoordinadorEntity coordinador){
+        throw new java.lang.UnsupportedOperationException("Not suported yet");
+    }
+    
+    public CoordinadorEntity modify(CoordinadorEntity coordinador){
+        throw new java.lang.UnsupportedOperationException("Not suported yet");
+    }
+    
+    public CoordinadorEntity delete(CoordinadorEntity coordinador){
+        throw new java.lang.UnsupportedOperationException("Not suported yet");
+    }
+}
