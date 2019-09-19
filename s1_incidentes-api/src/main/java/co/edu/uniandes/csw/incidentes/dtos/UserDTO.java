ef776dd0f5a674e50ba6a268a77f7067c150df66
Juan Camilo Castiblanco <jc.castiblancor@uniandes.edu.co>
2019-09-18 21:06:11 -0500

    se creo User DTO

---
+++ s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/dtos/UserDTO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.incidentes.dtos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author TAURET
+ */
+public class UserDTO implements Serializable{
+    
+    private String username;
+    
+    private String password;
+
+    public UserDTO() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    
+}
