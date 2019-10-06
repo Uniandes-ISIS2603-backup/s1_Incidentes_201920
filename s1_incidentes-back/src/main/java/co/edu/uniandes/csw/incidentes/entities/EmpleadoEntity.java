a8ba672d5dfa9e67da801732b2ea4a80339415c4
Julian Jaimes <>
2019-10-06 11:32:19 -0500

    Se corrigio empleadoentity para que cumpla con el nuevo uml

--- s1_incidentes-back/src/main/java/co/edu/uniandes/csw/incidentes/entities/EmpleadoEntity.java
+++ s1_incidentes-back/src/main/java/co/edu/uniandes/csw/incidentes/entities/EmpleadoEntity.java
@@ -16,8 +16,6 @@ import javax.persistence.Entity;
 @Entity
 public class EmpleadoEntity extends User2Entity implements Serializable  {
     private String nombre;
-    private String tipo;
-    private Boolean incidenteAbierto;
     private Integer numIncidentes;
 
     /**
@@ -32,34 +30,6 @@ public class EmpleadoEntity extends User2Entity implements Serializable  {
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    /**
-     * @return the tipo
-     */
-    public String getTipo() {
-        return tipo;
-    }
-
-    /**
-     * @param tipo the tipo to set
-     */
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    /**
-     * @return the incidenteAbierto
-     */
-    public Boolean getIncidenteAbierto() {
-        return incidenteAbierto;
-    }
-
-    /**
-     * @param incidenteAbierto the incidenteAbierto to set
-     */
-    public void setIncidenteAbierto(Boolean incidenteAbierto) {
-        this.incidenteAbierto = incidenteAbierto;
     }
 
     /**
