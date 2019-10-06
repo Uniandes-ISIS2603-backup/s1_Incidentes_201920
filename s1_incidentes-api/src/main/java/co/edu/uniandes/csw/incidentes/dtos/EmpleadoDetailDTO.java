7e1a082fc97cb48083a9ec53cbd3f43cf5bf4821
Julian Jaimes <>
2019-10-06 12:47:43 -0500

    EmpleadoDetailDTO listo

--- s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/dtos/EmpleadoDetailDTO.java
+++ s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/dtos/EmpleadoDetailDTO.java
@@ -5,17 +5,64 @@
  */
 package co.edu.uniandes.csw.incidentes.dtos;
 
+import co.edu.uniandes.csw.incidentes.entities.EmpleadoEntity;
+import co.edu.uniandes.csw.incidentes.entities.IncidenteEntity;
 import java.io.Serializable;
+import java.util.List;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import java.util.ArrayList;
 
 /**
  *
- * @author xx
+ * @author Julian Jaimes
  */
 public class EmpleadoDetailDTO extends EmpleadoDTO implements Serializable{
 
     public EmpleadoDetailDTO() {
         super();
     }
-    
-    
+
+    private List<IncidenteDTO> incidentes;
+
+    public EmpleadoDetailDTO(EmpleadoEntity empleadoEntity) {
+        super(empleadoEntity);
+        if (empleadoEntity != null) {
+            if (empleadoEntity.getIncidentes() != null) {
+                incidentes = new ArrayList<>();
+                for (IncidenteEntity entityIncidente : empleadoEntity.getIncidentes()) {
+                    incidentes.add(new IncidenteDTO(entityIncidente));
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public EmpleadoEntity toEntity() {
+        EmpleadoEntity empleadoEntity = super.toEntity();
+
+        if (incidentes != null) {
+            List<IncidenteEntity> incidenteEntity = new ArrayList<>();
+            for (IncidenteDTO dtoIncidente : incidentes) {
+                incidenteEntity.add(dtoIncidente.toEntity());
+            }
+            empleadoEntity.setIncidentes(incidenteEntity);
+        }
+
+        return empleadoEntity;
+    }
+
+    public List<IncidenteDTO> getIncidentes() {
+        return incidentes;
+    }
+
+    public void setIncidentes(List<IncidenteDTO> incidentes) {
+        this.incidentes = incidentes;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
 }
