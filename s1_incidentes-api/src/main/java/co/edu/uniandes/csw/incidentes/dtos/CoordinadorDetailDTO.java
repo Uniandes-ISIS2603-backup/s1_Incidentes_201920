a5921aa12c36b3e38fd0ddfd9043d4b0bab894ef
Juan Camilo Castiblanco <jc.castiblancor@uniandes.edu.co>
2019-10-06 10:44:02 -0500

    add asociacion incidentes-coordinador

--- s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/dtos/CoordinadorDetailDTO.java
+++ s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/dtos/CoordinadorDetailDTO.java
@@ -6,10 +6,16 @@
 package co.edu.uniandes.csw.incidentes.dtos;
 
 import co.edu.uniandes.csw.incidentes.entities.CoordinadorEntity;
+import co.edu.uniandes.csw.incidentes.entities.IncidenteEntity;
+import co.edu.uniandes.csw.incidentes.entities.TecnicoEntity;
 import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 import java.io.Serializable;
 import java.util.ArrayList;
+import static java.util.Collections.list;
+import java.util.List;
 import java.util.logging.Level;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  *
@@ -17,44 +23,75 @@ import java.util.logging.Level;
  */
 public class CoordinadorDetailDTO extends CoordinadorDTO implements Serializable {
 
-    //private list <TecnicoDTO> tecnicos;
+    private List<TecnicoDTO> tecnicos;
+
+    private List<IncidenteDTO> incidentes;
+
     public CoordinadorDetailDTO() {
         super();
     }
 
     public CoordinadorDetailDTO(CoordinadorEntity coordinadorEntity) {
         super(coordinadorEntity);
-        if (coordinadorEntity.getTecnicos() != null) {
-            //tecnicos = new ArrayList<>();
-            //for (TecnicoEntity entityTecnico : coordinadorEntity.getTecnicos()) {
-            //    tecnicos.add(new TecnicoDTO(entityTecnico));
-            //}
+        if (coordinadorEntity != null) {
+            if (coordinadorEntity.getTecnicos() != null) {
+                tecnicos = new ArrayList<>();
+                for (TecnicoEntity entityTecnico : coordinadorEntity.getTecnicos()) {
+                    tecnicos.add(new TecnicoDTO(entityTecnico));
+                }
+            }
+
+            if (coordinadorEntity.getIncidentes() != null) {
+                incidentes = new ArrayList<>();
+                for (IncidenteEntity entityIncidente : coordinadorEntity.getIncidentes()) {
+                    incidentes.add(new IncidenteDTO(entityIncidente));
+                }
+            }
         }
+
     }
 
     @Override
     public CoordinadorEntity toEntity() {
         CoordinadorEntity coordinadorEntity = super.toEntity();
-        /*
+
         if (tecnicos != null) {
             List<TecnicoEntity> tecnicoEntity = new ArrayList<>();
-            for (TecnicoDTO dtoTecnico : getTecnicos()) {
-                tecnicoEntity.add(dtotecnico.toEntity());
+            for (TecnicoDTO dtoTecnico : tecnicos) {
+                tecnicoEntity.add(dtoTecnico.toEntity());
             }
             coordinadorEntity.setTecnicos(tecnicoEntity);
         }
-         */
+
+        if (incidentes != null) {
+            List<IncidenteEntity> incidenteEntity = new ArrayList<>();
+            for (IncidenteDTO dtoIncidente : incidentes) {
+                incidenteEntity.add(dtoIncidente.toEntity());
+            }
+            coordinadorEntity.setIncidentes(incidenteEntity);
+        }
 
         return coordinadorEntity;
     }
 
-    /*
-     public <TecnicoDTO> getTecnicos() {
+    public List<TecnicoDTO> getTecnicos() {
         return tecnicos;
     }
 
-    public void setTecnicos(<TecnicoDTO> tecnicos) {
+    public void setTecnicos(List<TecnicoDTO> tecnicos) {
         this.tecnicos = tecnicos;
     }
-     */
+
+    public List<IncidenteDTO> getIncidentes() {
+        return incidentes;
+    }
+
+    public void setIncidentes(List<IncidenteDTO> incidentes) {
+        this.incidentes = incidentes;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
 }
