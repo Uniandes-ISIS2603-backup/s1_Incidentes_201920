09eb544db7407bd7bcf7f975820f1b0aead0083e
Diego Fernando Forero Cruz <df.foreroc@uniandes.edu.co>
2019-10-08 09:55:20 -0500

    Actualizacion de IncidenteResource

--- s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/resources/IncidenteResource.java
+++ s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/resources/IncidenteResource.java
@@ -106,12 +106,12 @@ public class IncidenteResource {
         incidenteLogic.deleteIncidente(incidenteId);
         LOGGER.info("IncidenteResource deleteIncidente: output: void");
     }
-     //@Path("{incidentesId: \\d+}/actuaciones")
-    //public Class<ActuacionesResource> getActuacionesResource(@PathParam("incidentesId") Long incidentesId) {
-     ////   if (incidenteLogic.getIncidente(incidentesId) == null) {
-           // throw new WebApplicationException("El recurso /incidentes/" + incidentesId + " no existe.", 404);
-      //  }
-      ////  return ActuacionesResource.class;
-    //}
+    @Path("{incidentesId: \\d+}/actuaciones")
+    public Class<IncidenteActuacionResource> getIncidenteActuacionResource(@PathParam("incidentesId") Long incidentesId) {
+        if (incidenteLogic.getIncidente(incidentesId) == null) {
+            throw new WebApplicationException("El recurso /incidentes/" + incidentesId + " no existe.", 404);
+        }
+        return IncidenteActuacionResource.class;
+    }
 
 }
