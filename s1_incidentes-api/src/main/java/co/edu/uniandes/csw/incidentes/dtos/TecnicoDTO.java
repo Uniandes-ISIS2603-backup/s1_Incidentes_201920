2207edfefef2c5546f6fcfc60729005762012daa
Estudiante <da.silvaa>
2019-10-09 19:56:05 -0500

    arreglo errores

--- s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/dtos/TecnicoDTO.java
+++ s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/dtos/TecnicoDTO.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,12 +9,14 @@ package co.edu.uniandes.csw.incidentes.dtos;
 import co.edu.uniandes.csw.incidentes.entities.TecnicoEntity;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import co.edu.uniandes.csw.incidentes.entities.TecnicoEntity;
+import java.io.Serializable;
 
 /**
  *
  * @author Estudiante
  */
-public class TecnicoDTO {
+public class TecnicoDTO implements Serializable{
        
     private Long id;
     
@@ -60,37 +62,3 @@ public class TecnicoDTO {
     }
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package co.edu.uniandes.csw.incidentes.dtos;
-
-import co.edu.uniandes.csw.incidentes.entities.TecnicoEntity;
-import java.io.Serializable;
-
-/**
- *
- * @author xx
- */
-public class TecnicoDTO implements Serializable {
-
-    public TecnicoDTO() {
-
-    }
-
-    public TecnicoDTO(TecnicoEntity tecnicoEntity) {
-        if (tecnicoEntity != null) {
-
-        }
-    }
-
-    public TecnicoEntity toEntity() {
-        TecnicoEntity tecnicoEntity = new TecnicoEntity();
-        
-        return tecnicoEntity;
-    }
-}
->>>>>>> 79b9c42217a1f64487bcff923c0a08dfe3d94bba
--- s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/dtos/TecnicoDetailDTO.java
+++ s1_incidentes-api/src/main/java/co/edu/uniandes/csw/incidentes/dtos/TecnicoDetailDTO.java
@@ -1,38 +1,20 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.incidentes.dtos;
+import java.io.Serializable;
 
 /**
  *
  * @author Estudiante
  */
-public class TecnicoDetailDTO {
-    
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package co.edu.uniandes.csw.incidentes.dtos;
-
-import java.io.Serializable;
-
-/**
- *
- * @author TAURET
- */
-public class TecnicoDetailDTO extends TecnicoDTO implements Serializable{
-
+public class TecnicoDetailDTO extends TecnicoDTO implements Serializable {
     public TecnicoDetailDTO() {
         super();
     }
-    
-    
 }
->>>>>>> 79b9c42217a1f64487bcff923c0a08dfe3d94bba
+
+
+
