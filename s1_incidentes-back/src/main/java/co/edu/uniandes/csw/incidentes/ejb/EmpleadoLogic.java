dd275fcdec3d59e3c90def114401d7eae4fc629e
Julian Jaimes <>
2019-10-10 02:05:03 -0500

    documentacion

--- s1_incidentes-back/src/main/java/co/edu/uniandes/csw/incidentes/ejb/EmpleadoLogic.java
+++ s1_incidentes-back/src/main/java/co/edu/uniandes/csw/incidentes/ejb/EmpleadoLogic.java
@@ -13,8 +13,10 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 /**
- *
- * @author Daniel Reyes
+ *Clase que implementa la conexion con la persistencia para la entidad
+ * empleado.
+ * 
+ * @author Julian Jaimes
  */
 
 @Stateless
@@ -23,9 +25,16 @@ public class EmpleadoLogic {
     @Inject
     private EmpleadoPersistence persistence;
     
+    /**
+     * Crea un empleado en la persistencia.
+     *
+     * @param empleado La entidad que representa el empleado a persistir.
+     * @return La entidad del empleado luego de persistirla.
+     * @throws BusinessLogicException Si se incumple una regla de negocio.
+     */
     public EmpleadoEntity createEmpleado(EmpleadoEntity empleado) throws BusinessLogicException{
         
-        if(empleado.getNombre() == null || empleado.getNombre() == ""){
+        if(empleado.getNombre() == null || empleado.getNombre().equals("")){
             throw new BusinessLogicException("El nombre es nulo o vacio");
         }
         if(empleado.getNumIncidentes() < 0){
@@ -51,21 +60,46 @@ public class EmpleadoLogic {
         return empleado;
     }
     
+    /**
+     * Obtener todos los Empleados existentes en la base de datos.
+     *
+     * @return una lista de Empleados.
+     */
     public List<EmpleadoEntity> getEmpleados() {
         List<EmpleadoEntity> lista = persistence.listAll();
         return lista;
     }
     
+    /**
+     * Obtener un empleado por medio de su id.
+     *
+     * @param empleadoId: id del empleado para ser buscado.
+     * @return el empleado solicitado por medio de su id.
+     */
     public EmpleadoEntity getEmpleado(Long empleadoId) {
         EmpleadoEntity empleadoEntity = persistence.find(empleadoId);
         return empleadoEntity;
     }
     
+    /**
+     * Actualizar un empleado.
+     *
+     * @param empleadoId: id del empleado para buscarlo en la base de
+     * datos.
+     * @param empleado: empleado con los cambios para ser actualizado,
+     * por ejemplo el nombre.
+     * @return el empleado con los cambios actualizados en la base de datos.
+     */
     public EmpleadoEntity updateEmpleado(Long empleadoId, EmpleadoEntity empleado) {
         EmpleadoEntity newEmpleadoEntity = persistence.update(empleado);
         return newEmpleadoEntity;
     }
     
+    /**
+     * Borrar un empleado
+     *
+     * @param empleadoId: id del emleado a borrar
+     */
     public void deleteEmpleado(Long empleadoId){
         persistence.delete(empleadoId);
     }
